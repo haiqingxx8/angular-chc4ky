6aaa7274175255dece64f7159f336b263f81cdf6
haiqingxx8 <53305314+haiqingxx8@users.noreply.github.com>
2023-04-10 13:11:33 +0800

    3 files modified, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,18 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent },{ path: 'products/:productId', component: ProductDetailsComponent }]),
+    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductAlertsComponent,
     ProductListComponent,
-    ProductDetailsComponent,
   ],
   bootstrap: [AppComponent],
 })
